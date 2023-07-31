c8bfa3e8413b2cd6bec971895f942879447ca187
Anshul Kanwar <me@anshulkanwar.com>
2023-07-31 23:01:37 +0530

    add create and join room buttons

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,13 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main>
+    <main className='grid place-items-center h-full'>
+      <div className='mt-32'>
+        <h1 className='text-center'>Play Tic-Tac-Toe with friends</h1>
+        <div className='mt-5 flex gap-5'>
+          <button className='p-3 rounded-xl bg-sky-600 hover:bg-sky-700'>Create Room</button>
+          <button className='p-3 rounded-xl bg-sky-600 hover:bg-sky-700'>Join Room</button>
+        </div>
+      </div>
     </main>
   )
 }
