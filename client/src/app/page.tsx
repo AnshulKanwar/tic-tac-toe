44c74014a31a8bb23c0e69ea1f705b7ec658cc12
Anshul Kanwar <me@anshulkanwar.com>
2023-08-01 11:26:24 +0530

    Split page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,14 @@
+import CreateRoomButton from "@/components/createRoomButton";
+import JoinRoomButton from "@/components/joinRoomButton";
+
 export default function Home() {
   return (
     <main className='grid place-items-center h-full'>
       <div className='mt-32'>
         <h1 className='text-center'>Play Tic-Tac-Toe with friends</h1>
         <div className='mt-5 flex gap-5'>
-          <button className='p-3 rounded-xl bg-sky-600 hover:bg-sky-700'>Create Room</button>
-          <button className='p-3 rounded-xl bg-sky-600 hover:bg-sky-700'>Join Room</button>
+          <CreateRoomButton />
+          <JoinRoomButton />
         </div>
       </div>
     </main>
---
+++ client/src/components/joinRoomButton.tsx
@@ -0,0 +1,5 @@
+export default function JoinRoomButton() {
+  return (
+    <button className='p-3 rounded-xl bg-sky-600 hover:bg-sky-700'>Join Room</button>
+  )
+}
