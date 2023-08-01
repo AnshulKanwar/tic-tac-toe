d0494f9c21a8f8f7c8bf2576eaae9baeca5d84de
Anshul Kanwar <me@anshulkanwar.com>
2023-08-01 15:03:03 +0530

    handle error when joining non existent room

--- server/src/gameManager.ts
+++ server/src/gameManager.ts
@@ -5,19 +5,23 @@ export default class GameManager {
   rooms: { [id: string]: Room } = {};
 
   createRoom() {
-    const roomId = uuid()
-    const room = new Room()
-    this.rooms[roomId] = room
+    const roomId = uuid();
+    const room = new Room();
+    this.rooms[roomId] = room;
 
-    return roomId
+    return roomId;
   }
 
   joinRoom(roomId: string) {
-    let playerId = this.rooms[roomId].joinRoom()
-    return playerId
+    if (this.rooms[roomId]) {
+      let playerId = this.rooms[roomId].joinRoom();
+      return playerId;
+    } else {
+      throw Error("Room does not exist")
+    }
   }
 
   playTurn(roomId: string, playerId: string) {
-    this.rooms[roomId].playTurn(playerId)
+    this.rooms[roomId].playTurn(playerId);
   }
 }
