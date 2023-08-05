9a2cfa4c1688baca5eb0f3a837c8e7695e4bc11c
Anshul Kanwar <me@anshulkanwar.com>
2023-08-05 22:36:20 +0530

    check for win

--- server/src/room.ts
+++ server/src/room.ts
@@ -70,20 +70,108 @@ export default class Room {
         this.turn = this.player1;
       }
 
-      this.player1?.ws.send(
-        JSON.stringify({
-          type: "playTurn",
-          turn: this.turn?.id,
-          state: this.state,
-        })
-      );
-      this.player2?.ws.send(
-        JSON.stringify({
-          type: "playTurn",
-          turn: this.turn?.id,
-          state: this.state,
-        })
-      );
+      const result = this.checkWin();
+      if (result) {
+        if (result === "draw") {
+          this.player1?.ws.send(
+            JSON.stringify({
+              type: "gameOver",
+              result,
+              state: this.state,
+            })
+          );
+          this.player2?.ws.send(
+            JSON.stringify({
+              type: "gameOver",
+              result,
+              state: this.state,
+            })
+          );
+        } else {
+          this.player1?.ws.send(
+            JSON.stringify({
+              type: "gameOver",
+              result: "win",
+              winner: result.id,
+              state: this.state,
+            })
+          );
+          this.player2?.ws.send(
+            JSON.stringify({
+              type: "gameOver",
+              result: "win",
+              winner: result.id,
+              state: this.state,
+            })
+          );
+        }
+      } else {
+        this.player1?.ws.send(
+          JSON.stringify({
+            type: "playTurn",
+            turn: this.turn?.id,
+            state: this.state,
+          })
+        );
+        this.player2?.ws.send(
+          JSON.stringify({
+            type: "playTurn",
+            turn: this.turn?.id,
+            state: this.state,
+          })
+        );
+      }
     }
+  }
+
+  private checkWin(): Player | "draw" | null {
+    // check row
+    this.state.forEach((row) => {
+      if (row[0] === row[1] && row[1] === row[2] && row[2] === "X") {
+        return this.player1;
+      } else if (row[0] === row[1] && row[1] === row[2] && row[2] === "O") {
+        return this.player2;
+      }
+    });
+
+    // check column
+    for (let i = 0; i <= 2; i++) {
+      if (
+        this.state[0][i] === this.state[1][i] &&
+        this.state[1][i] === this.state[2][i]
+      ) {
+        if (this.state[0][i] === "X") {
+          return this.player1;
+        } else if (this.state[0][i] === "O") {
+          return this.player2;
+        }
+      }
+    }
+
+    // check diagonal
+    if (this.state[0][0] === this.state[1][1] && this.state[1][1] === this.state[2][2]) {
+      if (this.state[0][0] === "X") {
+        return this.player1;
+      } else if (this.state[0][0] === "O") {
+        return this.player2;
+      }
+    }
+
+    if (this.state[0][2] === this.state[1][1] && this.state[1][1] === this.state[2][0]) {
+      if (this.state[0][2] === "X") {
+        return this.player1;
+      } else if (this.state[0][2] === "O") {
+        return this.player2;
+      }
+    }
+
+    let isDraw = true
+    this.state.forEach(row => {
+      if (row.includes("-")) {
+        isDraw = false
+      }
+    })
+
+    return isDraw ? "draw" : null;
   }
 }
